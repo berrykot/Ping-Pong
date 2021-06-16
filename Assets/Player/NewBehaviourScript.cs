b1b46893c529476a2193dd5b2673c00f9436d4b8
berrykot <berrykot@gmail.com>
2021-06-16 22:00:43 +0300

    Ограничил скорость шарика

---
+++ Assets/Player/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+using Bolt;
+using UnityEngine;
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    void FixedUpdate()
+    {
+        var x = 0;
+    }
+
+    void OnMouseDown()
+    {
+        //CustomEvent.Trigger(this.gameObject, "Fuck10");
+    }
+
+    
+    
+    
+}
